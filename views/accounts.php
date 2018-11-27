4d21c11688ea509fe79455cc8e5b53f88b101726
laurent <w32.tsim@gmail.com>
2018-11-27 00:20:49 -0500

    removed view details button

--- views/accounts.php
+++ views/accounts.php
@@ -37,7 +37,6 @@ foreach ($db->getAccountsByClient($_SESSION['client']['id']) as $account) {
             <h5 class="card-title"><?php echo ucfirst($account['type']); ?></h5>
             <p class="card-text">Balance: $<?php echo ucfirst($account['balance']); ?></p>
             <p class="card-text">Account Category: <?php echo ucfirst($account['category']); ?></p>
-            <a href="#" class="btn btn-primary">View Details</a>
         </div>
     </div>
     </br>
